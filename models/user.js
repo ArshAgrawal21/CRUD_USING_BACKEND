bbf98e3b1ab3867db2c4f6081e45597e3079eae9
ArshAgrawal21 <aaersha@gmail.com>
2025-02-09 14:04:44 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const name= require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/testapp1`);
+
+const userSchema = new mongoose.Schema({
+    image:String,
+    email:String,
+    name:String
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+
